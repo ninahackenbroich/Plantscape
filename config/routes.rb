31e314c9f249cb723a722bba2b34f6c35982f9a5
Kimberly Dohmen <kimberly.dohmen@web.de>
2020-08-25 14:22:32 +0200

    changed fonts to the choosen one, recovered the colors for the error messages in the body etc.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :jungles do
-    resources :jungleplants, only:[:show, :] do
-    	resources :plants, only:[:show, :]
+    resources :jungleplants, only:[:show] do
+    	resources :plants, only:[:show]
     end
   end
 
