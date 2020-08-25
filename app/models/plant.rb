ee5f713ef94ec3a8b4b6a18c7cfa58a5cd696079
Nina Hackenbroich <66108126+ninahackenbroich@users.noreply.github.com>
2020-08-25 12:41:58 +0200

    Update plant.rb

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -2,10 +2,5 @@ class Plant < ApplicationRecord
 
   #adding VALIDATION
   validates :name, presence: true
-  validates :water
-  validates :light
-  validates :soil
-  validates :fertilizer
-  validates :repotting
 
 end
