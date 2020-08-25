72ceb8403590febacad91c03f72771b24f7bec17
Celine Chambre <chambre.celine@gmail.com>
2020-08-25 12:29:13 +0200

    added routes as comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :jungles do
+    resources :jungleplants, only:[:show, :] do
+    	resources :plants, only:[:show, :]
+    end
+  end
+
+  resources :bookings, only:[:create, :new, :show, :index, :destroy]
 end
