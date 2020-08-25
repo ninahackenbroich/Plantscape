566566eecc70076459f4619d5a963c7b5405580d
raffaele triggiano <triggiano.r@gmail.com>
2020-08-25 12:20:58 +0200

    adding validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,9 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :jungle
   has_many :waterings
+
+  #adding VALIDATION
+  validates :number_of_plants, presence: true
+  validates :key, presence: true
+  validates :price, presence: true
 end
--- app/models/jungle.rb
+++ app/models/jungle.rb
@@ -1,4 +1,9 @@
 class Jungle < ApplicationRecord
   belongs_to :booking, optional: true
   belongs_to :user
+
+  #adding VALIDATION
+  validates :name, presence: true, length: { minimum: 4,
+    too_short: "4 characters is the minimum allowed" }
+  validates :address, presence: true
 end
