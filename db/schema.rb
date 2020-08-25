747a34a3204241c95b7ea59c5b216d4f28d4610f
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2020-08-25 12:18:18 +0200

    added index method for plants

--- db/schema.rb
+++ db/schema.rb
@@ -63,10 +63,8 @@ ActiveRecord::Schema.define(version: 2020_08_24_151030) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "flora.flower@gmail.com", null: false
-    t.string "encrypted_password", default: "********", null: false
-    t.string "first_name", default: "Flora", null: false
-    t.string "last_name", default: "Flower", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
