c5bd9d139c5c8b1a32fd1d1fbce4bdf2bb7271e0
raffaele triggiano <triggiano.r@gmail.com>
2020-08-26 15:46:18 +0200

    adding validations for all the attribute of a plant

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -5,7 +5,15 @@ class Plant < ApplicationRecord
 
   #adding VALIDATION
   validates :name, presence: true
-  validates :water, presence: true, inclusion: { in: ["every day", "once per week", "rare"],
+  validates :water, presence: true, inclusion: { in: ["Every day", "Once per week", "Rare"],
+    message: "dosen't accept %{value} as option" }
+  validates :light, presence: true, inclusion: { in: ["Direct light", "Shodow is better", "Everywhere"],
+    message: "dosen't accept %{value} as option" }
+  validates :soil, presence: true, inclusion: { in: ["Sand", "Silt", "Clay", "Mixed soil"],
+    message: "dosen't accept %{value} as option" }
+  validates :fertilizer, presence: true, inclusion: { in: ["Spring-Summer: Once every 2 weeks", "Spring-Summer: Once every week", "Whole Year: Once every month"],
+    message: "dosen't accept %{value} as option" }
+  validates :repotting, presence: true, inclusion: { in: ["Every 12 to 18 months: Repotting in a bigger pot", "Every 8 to 12 months: It likes medium pots", "Avoid repotting"],
     message: "dosen't accept %{value} as option" }
 
 end
