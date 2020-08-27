3fe8242830499c6ebd28644108def47aca66179e
Kimberly Dohmen <kimberly.dohmen@web.de>
2020-08-27 12:21:53 +0200

    Methods for the shows

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,6 +4,7 @@ class DashboardsController < ApplicationController
     @bookings = policy_scope(Booking)
     @user = current_user
     @bookings = Booking.where(user_id: @user)
+    @jungles = Jungle.where(user_id: @user)
     authorize @bookings
   end
 end
