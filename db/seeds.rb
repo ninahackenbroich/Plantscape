b1fe8e70b7299f8cba1c0d2d8e58badb4e45f5f0
raffaele triggiano <triggiano.r@gmail.com>
2020-08-27 10:22:50 +0200

    commenting user out and user destroy all

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,44 +1,44 @@
 require "faker"
 
 puts "Cleaning database..."
-User.destroy_all
+# User.destroy_all
 Plant.destroy_all
 
-puts "Creating a user"
-user1 = User.create!(
-  email: "hackenbroich.nina@gmail.com",
-  first_name:"Nina",
-  last_name: "Hackenbroich",
-  password: "123456",
-  admin: true,
-  )
+# puts "Creating a user"
+# user1 = User.create!(
+#   email: "hackenbroich.nina@gmail.com",
+#   first_name:"Nina",
+#   last_name: "Hackenbroich",
+#   password: "123456",
+#   admin: true,
+#   )
 
-puts "Creating a user"
-user2 = User.create!(
-  email: "triggiano.r@gmail.com",
-  first_name:"Raffaele",
-  last_name: "Triggiano",
-  password: "123456",
-  admin: true,
-  )
+# puts "Creating a user"
+# user2 = User.create!(
+#   email: "triggiano.r@gmail.com",
+#   first_name:"Raffaele",
+#   last_name: "Triggiano",
+#   password: "123456",
+#   admin: true,
+#   )
 
-puts "Creating a user"
-user3 = User.create!(
-  email: "chambre.celine@gmail.com",
-  first_name:"Celine",
-  last_name: "Chambre",
-  password: "123456",
-  admin: true,
-  )
+# puts "Creating a user"
+# user3 = User.create!(
+#   email: "chambre.celine@gmail.com",
+#   first_name:"Celine",
+#   last_name: "Chambre",
+#   password: "123456",
+#   admin: true,
+#   )
 
-puts "Creating a user"
-user4 = User.create!(
-  email: "kimberly.dohmen@web.de",
-  first_name:"Kim",
-  last_name: "Dohmen",
-  password: "123456",
-  )
-  
+# puts "Creating a user"
+# user4 = User.create!(
+#   email: "kimberly.dohmen@web.de",
+#   first_name:"Kim",
+#   last_name: "Dohmen",
+#   password: "123456",
+#   )
+
 
 
 puts "Creating plants"
