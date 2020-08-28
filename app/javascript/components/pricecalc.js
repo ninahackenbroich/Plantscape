4893cbdfeddea23ee476f531566a79c5c38632d9
Celine Chambre <chambre.celine@gmail.com>
2020-08-28 14:25:28 +0200

    js-loading-fix

--- app/javascript/components/pricecalc.js
+++ app/javascript/components/pricecalc.js
@@ -1,73 +1,72 @@
-
-
-
-
 const initPrice = () => {
   let finalprice = 0;
   const finalDiv = document.querySelector(".finalprice");
   const inputPrice = document.querySelector(".hiddenprice");
   const sliderOutput = document.querySelector("#number_of_plants");
-  let sliderValue = Number(sliderOutput.innerHTML);
   let base_price = 29.99;
   const sliderInput = document.querySelector(".slider")
-  sliderInput.addEventListener('change', () => {
+  
+  if (finalDiv) {
     let sliderValue = Number(sliderOutput.innerHTML);
-    if (sliderValue > 60 ) {
-      base_price = 19.99;
-    } else if (sliderValue > 40 ) {
-      base_price = 15.99;
-    } else if (sliderValue > 20 ) {
-      base_price = 10.99;
-    } else if (sliderValue > 10 ) {
-      base_price = 7.99;
-    } else if (sliderValue > 5 ) {
-      base_price = 5.99;
-    } else if (sliderValue > 0 ) {
-      base_price = 2.99;
-    } else {
-      base_price = 1.99;
-    };
-    console.log(base_price); 
-    finalprice = base_price;
-    console.log(finalprice);
-    finalDiv.innerText = finalprice + " €";
-    inputPrice.value = finalprice;
-  });
-
-  const calform = document.querySelector('#watering-days-calendar');
-    let price;
-    calform.addEventListener('change', (e) => {
-      let selectedDays = document.querySelectorAll('.selected');
-      price = (parseFloat(base_price).toFixed(2) * parseInt(selectedDays.length));
-      console.log(price);
-      finalprice = price;
+    sliderInput.addEventListener('change', () => {
+      let sliderValue = Number(sliderOutput.innerHTML);
+      if (sliderValue > 60 ) {
+        base_price = 19.99;
+      } else if (sliderValue > 40 ) {
+        base_price = 15.99;
+      } else if (sliderValue > 20 ) {
+        base_price = 10.99;
+      } else if (sliderValue > 10 ) {
+        base_price = 7.99;
+      } else if (sliderValue > 5 ) {
+        base_price = 5.99;
+      } else if (sliderValue > 0 ) {
+        base_price = 2.99;
+      } else {
+        base_price = 1.99;
+      };
+      console.log(base_price); 
+      finalprice = base_price;
       console.log(finalprice);
       finalDiv.innerText = finalprice + " €";
       inputPrice.value = finalprice;
-  });
-
-  const addons = document.querySelectorAll('.form-check-input');
-  
-  // const dustingservice = document.querySelector('#booking_dusting_service');
-  // const cuttingservice = document.querySelector('#booking_cutting_service');
-  // const repottingservice = document.querySelector('#booking_repotting_service');
-  console.log(addons);
-  let addonprice = 0;
-  addons.forEach((addon) => {
-    addon.addEventListener('change', (e) => {
-      if (e.currentTarget.checked) {
-        addonprice += 10;
-        finalprice += 10;
-      } else {
-        addonprice -= 10;
-        finalprice -= 10;
-      }
-      // let price = (parseInt(base_price) * parseInt(selectedDays.length)) + 10;
-      console.log(addonprice);
-      finalDiv.innerText = finalprice + " €";
-      inputPrice.value = finalprice;
     });
-  });
+
+    const calform = document.querySelector('#watering-days-calendar');
+      let price;
+      calform.addEventListener('change', (e) => {
+        let selectedDays = document.querySelectorAll('.selected');
+        price = (parseFloat(base_price).toFixed(2) * parseInt(selectedDays.length));
+        console.log(price);
+        finalprice = price;
+        console.log(finalprice);
+        finalDiv.innerText = finalprice + " €";
+        inputPrice.value = finalprice;
+    });
+
+    const addons = document.querySelectorAll('.form-check-input');
+    
+    // const dustingservice = document.querySelector('#booking_dusting_service');
+    // const cuttingservice = document.querySelector('#booking_cutting_service');
+    // const repottingservice = document.querySelector('#booking_repotting_service');
+    console.log(addons);
+    let addonprice = 0;
+    addons.forEach((addon) => {
+      addon.addEventListener('change', (e) => {
+        if (e.currentTarget.checked) {
+          addonprice += 10;
+          finalprice += 10;
+        } else {
+          addonprice -= 10;
+          finalprice -= 10;
+        }
+        // let price = (parseInt(base_price) * parseInt(selectedDays.length)) + 10;
+        console.log(addonprice);
+        finalDiv.innerText = finalprice + " €";
+        inputPrice.value = finalprice;
+      });
+    });
+  }
 };
 
 export {initPrice};
--- app/javascript/components/slider.js
+++ app/javascript/components/slider.js
@@ -1,19 +1,19 @@
-const sliderInput = document.querySelector(".slider")
-const sliderOutput = document.querySelector(".number-plants")
-const inputContainer = document.querySelector(".booking-question")
-
 const initSlider = () => {
-	sliderInput.addEventListener( "input", event => {
-		const sliderValue = Math.floor(sliderInput.value)
-		const sliderMin = sliderInput.min
-		const sliderMax = sliderInput.max
-		const valuePercentage = (100 * sliderValue) / sliderMax
-		// const newValue = Number(((sliderValue - sliderMin) * 100) / (sliderMax - sliderMin))
-		sliderOutput.innerHTML = sliderValue
-		sliderOutput.style.left = `(${(valuePercentage * inputContainer.width) / 100}%)` 
-		console.log(valuePercentage, sliderInput.style)
-
-	})
+	const sliderInput = document.querySelector(".slider")
+	const sliderOutput = document.querySelector(".number-plants")
+	const inputContainer = document.querySelector(".booking-question")
+	if (sliderInput) {
+		sliderInput.addEventListener( "input", event => {
+			const sliderValue = Math.floor(sliderInput.value)
+			const sliderMin = sliderInput.min
+			const sliderMax = sliderInput.max
+			const valuePercentage = (100 * sliderValue) / sliderMax
+			// const newValue = Number(((sliderValue - sliderMin) * 100) / (sliderMax - sliderMin))
+			sliderOutput.innerHTML = sliderValue
+			sliderOutput.style.left = `(${(valuePercentage * inputContainer.width) / 100}%)` 
+			console.log(valuePercentage, sliderInput.style)
+		})
+	}
 }
 
 export { initSlider }
