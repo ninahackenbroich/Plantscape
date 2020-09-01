215fcbb76d7113879f7e0ca6d2c006f1d7425062
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2020-09-01 14:06:25 +0200

    chat done

--- app/javascript/components/anchor.js
+++ app/javascript/components/anchor.js
@@ -1,13 +1,15 @@
 const initAnchorOne = () => {
   const arrowOne = document.querySelector('.arrow-1');
   const el = document.querySelector('#watering-cal');
-  arrowOne.addEventListener('click', function () {
-    el.scrollIntoView({
-      behavior: "smooth",
-      block: "nearest",
-      inline: "end",
+  if (arrowOne) {
+      arrowOne.addEventListener('click', function () {
+      el.scrollIntoView({
+        behavior: "smooth",
+        block: "nearest",
+        inline: "end",
+      });
     });
-  });
+  }
 };
 
 export {initAnchorOne};
@@ -16,14 +18,15 @@ export {initAnchorOne};
 const initAnchorTwo = () => {
   const arrowTwo = document.querySelector('.arrow-2');
   const key = document.querySelector('#key');
-  
-  arrowTwo.addEventListener('click', function () {
+  if (arrowTwo) {
+      arrowTwo.addEventListener('click', function () {
       key.scrollIntoView({
         behavior: "smooth",
         block: "nearest",
         inline: "center",
     });
   });
+  }
 };
 
 export {initAnchorTwo};
@@ -32,14 +35,15 @@ export {initAnchorTwo};
 const initAnchorThree = () => {
   const arrowThree = document.querySelector('.arrow-3');
   const select = document.querySelector('#addon-select');
-  
-  arrowThree.addEventListener('click', function () {
+  if (arrowThree) {
+      arrowThree.addEventListener('click', function () {
     select.scrollIntoView({
       behavior: "smooth",
       block: "nearest",
       inline: "start",
     });
   });
+  }
 };
 
 export {initAnchorThree};
@@ -48,14 +52,15 @@ export {initAnchorThree};
 const initAnchorFour = () => {
   const arrowFour = document.querySelector('.arrow-4');
   const book = document.querySelector('#booking-btn');
-  
-  arrowFour.addEventListener('click', function () {
+  if (arrowFour) {
+     arrowFour.addEventListener('click', function () {
     book.scrollIntoView({
       behavior: "smooth",
       block: "nearest",
       inline: "end",
     });
-  });
+  }); 
+  }
 };
 
 export {initAnchorFour};
