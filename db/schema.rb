26f2024c421662c22928b9ea16265b9ce3554061
raffaele triggiano <triggiano.r@gmail.com>
2020-09-01 18:13:30 +0200

    label false and some chat form styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_09_01_124557) do
 
   # These are extensions that must be enabled in order to support this database
@@ -128,10 +127,10 @@ ActiveRecord::Schema.define(version: 2020_09_01_124557) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "flora.flower@gmail.com", null: false
-    t.string "encrypted_password", default: "********", null: false
-    t.string "first_name", default: "Flora", null: false
-    t.string "last_name", default: "Flower", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
