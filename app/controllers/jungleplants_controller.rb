af5a3d873fcf02a6db427fd146b8128441837385
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2020-09-03 09:52:37 +0200

    chat reverse fix

--- app/controllers/jungleplants_controller.rb
+++ app/controllers/jungleplants_controller.rb
@@ -1,6 +1,5 @@
 class JungleplantsController < ApplicationController
   before_action :set_jungle, only: [:show, :edit, :update, :destroy]
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @jungleplants = Jungleplant.all
