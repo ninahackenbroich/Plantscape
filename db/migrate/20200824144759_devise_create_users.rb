b153b1cdac1db6c1897b8cb3d686b0cfe8a157d0
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2020-09-09 21:39:28 +0200

    broken signup fixed

--- db/migrate/20200824144759_devise_create_users.rb
+++ db/migrate/20200824144759_devise_create_users.rb
@@ -6,8 +6,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false
       t.string :encrypted_password, null: false
-      t.string :first_name,         null: false
-      t.string :last_name,          null: false
+      t.string :first_name
+      t.string :last_name        
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
